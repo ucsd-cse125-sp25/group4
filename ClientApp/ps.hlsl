5aab1e50b2869716d71e224618e41c3318780da5
slayyden <erikek2014@gmail.com>
2025-05-12 16:42:15 -0700

    share header between hlsl files

--- ClientApp/ps.hlsl
+++ ClientApp/ps.hlsl
@@ -1,9 +1,4 @@
-// TODO: create a shared hlsl header
-struct PSInput
-{
-    float4 position : SV_POSITION;
-};
-
+#include "shaderShared.hlsli"
 float4 PSMain(PSInput input, uint id : SV_PrimitiveID) : SV_TARGET
 {
     return float4(1, 0, id/12.0, 1);
