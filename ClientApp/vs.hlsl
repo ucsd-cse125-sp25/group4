ff334bcdd99c0fd4cba72cd0074536451750303d
slayyden <erikek2014@gmail.com>
2025-05-12 17:16:10 -0700

    normal vectors!

--- ClientApp/vs.hlsl
+++ ClientApp/vs.hlsl
@@ -9,10 +9,15 @@ ConstantBuffer<PerDrawConstants> drawConstants : register(b1);
 
 PSInput VSMain(uint vid : SV_VertexID)
 {
-    StructuredBuffer<VertexPosition> vbuffer = ResourceDescriptorHeap[drawConstants.vbuffer_idx];
+    StructuredBuffer<VertexPosition> vbuffer = ResourceDescriptorHeap[drawConstants.vpos_idx];
     float4 position_homogeneous = float4(vbuffer[vid].position, 1);
+    
+    StructuredBuffer<VertexShadingData> shadebuffer = ResourceDescriptorHeap[drawConstants.vshade_idx];
+    float3 normal = shadebuffer[vid].normal;
+    
     PSInput result;
     result.position = mul(position_homogeneous, drawConstants.modelViewProject); // offset is visible outside of the struct
+    result.normal = normal;
 
     return result;
     
