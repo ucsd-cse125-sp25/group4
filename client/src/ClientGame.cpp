871a06d84789e783761b889f4783eb7472fff551
andronius-yang <androniusyang@gmail.com>
2025-05-14 14:16:17 -0700

    fixed dash bug, changed server executable name to bypass firewall, edited camera logic, verified dodge logic.

--- client/src/ClientGame.cpp
+++ client/src/ClientGame.cpp
@@ -242,7 +242,7 @@ bool ClientGame::processCameraInput()
 	if (!dx && !dy) return false;
 
 	yaw += -dx * MOUSE_SENS;
-	pitch += dy * MOUSE_SENS;
+	pitch += -dy * MOUSE_SENS; // invert y makes more sense
 	pitch = std::clamp(pitch,
 		XMConvertToRadians(-89.0f),
 		XMConvertToRadians(+89.0f));
